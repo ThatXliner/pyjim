89d39c0abe7caccfd557ac4e786cb95649b67ae4
ThatXliner <bryan.hu.cn@gmail.com>
2020-08-08 11:27:52 -0700

    Update SyncVersion.py

--- pyproj/SyncVersion.py
+++ pyproj/SyncVersion.py
@@ -16,34 +16,25 @@ def find_version_files(root_dir: str) -> List[Path]:
 
     """
     root_dir = str(root_dir) if not isinstance(root_dir, str) else root_dir
-    rd = Path(root_dir).expanduser()
+    rd = Path(root_dir).expanduser()  # NOTE: we should merge this with the above
     try:
         assert rd.exists() and rd.is_dir()
     except AssertionError:
         raise ValueError(
             "Root directory is invalid: it either does not exist or is not a directory"
-        )
-    version_files = []
+            )
 
-    def f():
+    def f(rd):
+        version_files = []
+        rd = Path(str(rd)).expanduser()
         with os.scandir(str(rd)) as scan_rd:
             for entry in scan_rd:
-                if not entry.name.startswith(".") and entry.is_dir():
-                    if entry.name.lower().strip("1234567890!@#$%^&*()_+-=") == "tests":
-                        continue
-
-    with os.scandir(str(rd)) as scan_rd:
-        for entry in scan_rd:
-            if not entry.name.startswith(".") and entry.is_dir():
-                if entry.name.lower().strip("1234567890!@#$%^&*()_+-=") == "tests":
-                    continue
-                else:
-                    with os.scandir(entry) as inner_dir:
-                        for myentry in inner_dir:
-                            if myentry.name == "__init__.py" and myentry.is_file():
-                                version_files.append(Path(Path(entry) / Path(myentry)))
-    return version_files
-
+                if not entry.name.startswith(".") and entry.is_dir() and not entry.name.lower().strip("1234567890!@#$%^&*()_+-=") == "tests":
+                    version_files.extend(f(entry.path))
+                elif myentry.name == "__init__.py" and myentry.is_file():
+                    version_files.append(Path(entry.path))
+        return version_files
+    return f(rd)
 
 def change_version(version_to_change_to: str, contents: str) -> str:
     match = re.search(
