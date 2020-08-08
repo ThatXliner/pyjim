1c793ba0b044edb74c46f55634612eedbf718a8b
theXliner <bryan.hu.cn@gmail.com>
2020-08-08 11:40:19 -0700

    Travis happy!

--- pyproj/SyncVersion.py
+++ pyproj/SyncVersion.py
@@ -15,28 +15,36 @@ def find_version_files(root_dir: str) -> List[Path]:
     :rtype: List[Path]
 
     """
-    root_dir = str(root_dir) if not isinstance(root_dir, str) else root_dir
-    rd = Path(root_dir).expanduser()  # NOTE: we should merge this with the above
+    root_dir = (
+        Path(str(root_dir)).expanduser()
+        if not isinstance(root_dir, str)
+        else Path(root_dir).expanduser()
+    )
     try:
-        assert rd.exists() and rd.is_dir()
+        assert root_dir.exists() and root_dir.is_dir()
     except AssertionError:
         raise ValueError(
             "Root directory is invalid: it either does not exist or is not a directory"
-            )
+        )
 
-    def f(rd):
+    def recursive_find(rd):
         version_files = []
         rd = Path(str(rd)).expanduser()
         with os.scandir(str(rd)) as scan_rd:
             for entry in scan_rd:
-                if not entry.name.startswith(".") and entry.is_dir() and not entry.name.lower().strip("1234567890!@#$%^&*()_+-=") == "tests":
-                    version_files.extend(f(entry.path))
-                elif myentry.name == "__init__.py" and myentry.is_file():
+                if entry.is_dir() and not (
+                    entry.name.startswith(".")
+                    or entry.name.lower().strip("1234567890!@#$%^&*()_+-=") == "tests"
+                ):
+                    version_files.extend(recursive_find(entry.path))
+                elif entry.name == "__init__.py" and entry.is_file():
                     version_files.append(Path(entry.path))
         return version_files
-    return f(rd)
 
-def change_version(version_to_change_to: str, contents: str) -> str:
+    return recursive_find(root_dir)
+
+
+def change_version(version_to_change_to: str, contents: str) -> str:  # noqa D103
     match = re.search(
         r"(.|\s)*(__version__\s*=\s*)(.*)(.|\s)*", contents, flags=re.IGNORECASE
     )
